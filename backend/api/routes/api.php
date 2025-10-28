84da0abd4fcccda2dccdf50b0b63584297f78518
beirs_dev <beirs-dev@proton.me>
2025-10-28 11:27:21 +0800

    removed unnecessary code

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -1,11 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return response()->json(['status' => 'success', 'message' => 'laravel api is running...']);
-});
-
 foreach (glob(__DIR__ . '/api/*.php') as $routeFile) {
     require_once $routeFile;
 }
