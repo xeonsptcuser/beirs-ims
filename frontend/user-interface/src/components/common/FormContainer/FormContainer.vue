6ce7bd6c65c63016f862342a55dca5f3527e15d9
beirs_dev <beirs-dev@proton.me>
2025-10-29 19:31:46 +0800

    fixed error message show

--- frontend/user-interface/src/components/common/FormContainer/FormContainer.vue
+++ frontend/user-interface/src/components/common/FormContainer/FormContainer.vue
@@ -9,9 +9,9 @@ defineProps({
 <template>
   <div class="min-vh-100 d-flex align-items-center justify-content-center bg-light">
     <div class="card shadow-sm"
-      :style="{ width: '100%', maxWidth: typeof maxWidth === 'number' ? `${maxWidth}px` : maxWidth, height: '380px' }">
+      :style="{ width: '100%', maxWidth: typeof maxWidth === 'number' ? `${maxWidth}px` : maxWidth }">
       <div class="card-body d-flex flex-column">
-        <h5 class="card-title text-center mb-0">Account Login</h5>
+        <h5 class="card-title text-center mb-3">Account Login</h5>
         <slot />
       </div>
     </div>
--- frontend/user-interface/src/views/LandingPage/LoginPage/LoginPage.vue
+++ frontend/user-interface/src/views/LandingPage/LoginPage/LoginPage.vue
@@ -2,7 +2,7 @@
 import FormContainer from "@/components/common/FormContainer/FormContainer.vue";
 import FormInput from "./components/FormInput.vue";
 import { useLogin } from "./composable/useLogin";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const {
   form,
@@ -28,6 +28,9 @@ const handleLogin = async () => {
   }
 }
 
+const filteredErrors = computed(() => {
+  return Object.values(errorMessages.value).filter(msg => msg.error && msg.error.trim() !== '');
+});
 </script>
 <template>
   <div class="my-4">
@@ -35,7 +38,7 @@ const handleLogin = async () => {
       <FormContainer>
         <div class="alert alert-danger" role="alert" v-show="hasError">
           <ul class="bg-white rounded p-2 mb-1 list-group-flush">
-            <li class="list-group-item mb-1" style="font-size: 13px;" v-for="message in errorMessages"><i
+            <li class="list-group-item mb-1" style="font-size: 13px;" v-for="message in filteredErrors"><i
                 class="bi bi-exclamation-circle-fill" style="font-size: 13px;"></i> {{ message.error
                 }}
             </li>
