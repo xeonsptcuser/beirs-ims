f7b73e33819c4719f3477dd8d5a548462fe18fa3
beirs_dev <beirs-dev@proton.me>
2025-10-29 19:47:59 +0800

    minor fix

---
+++ frontend/user-interface/src/components/common/WarningLabel/WarningLabel.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+
+defineProps({
+  hasError: {
+    type: Boolean,
+    required: false,
+    default: false
+  },
+  errors: {
+    type: Array<{ error: string }>,
+    required: true
+  }
+})
+
+</script>
+<template>
+  <div class="alert alert-danger" role="alert" v-show="hasError">
+    <ul class="bg-white rounded p-2 mb-1 list-group-flush">
+      <li class="list-group-item mb-1" style="font-size: 13px;" v-for="message in errors"><i
+          class="bi bi-exclamation-circle-fill" style="font-size: 13px;"></i> {{ message.error
+          }}
+      </li>
+    </ul>
+  </div>
+</template>
--- frontend/user-interface/src/views/LandingPage/LoginPage/LoginPage.vue
+++ frontend/user-interface/src/views/LandingPage/LoginPage/LoginPage.vue
@@ -3,6 +3,7 @@ import FormContainer from "@/components/common/FormContainer/FormContainer.vue";
 import FormInput from "./components/FormInput.vue";
 import { useLogin } from "./composable/useLogin";
 import { computed, ref } from "vue";
+import WarningLabel from "@/components/common/WarningLabel/WarningLabel.vue";
 
 const {
   form,
@@ -11,14 +12,16 @@ const {
   validateForm
 } = useLogin();
 
+// Shows error messages if true
 const hasError = ref(false);
 
+// This handles the login logic
 const handleLogin = async () => {
   try {
     const isValid = validateForm()
     if (isValid) {
-
-      console.log('SUCCESS:', isValid)
+      // PERFORM logic to login user here...
+      console.log("FORM: ", form)
       hasError.value = false;
     } else {
       hasError.value = true;
@@ -31,19 +34,13 @@ const handleLogin = async () => {
 const filteredErrors = computed(() => {
   return Object.values(errorMessages.value).filter(msg => msg.error && msg.error.trim() !== '');
 });
+
 </script>
 <template>
   <div class="my-4">
     <div class="bg-primary p-6 rounded">
       <FormContainer>
-        <div class="alert alert-danger" role="alert" v-show="hasError">
-          <ul class="bg-white rounded p-2 mb-1 list-group-flush">
-            <li class="list-group-item mb-1" style="font-size: 13px;" v-for="message in filteredErrors"><i
-                class="bi bi-exclamation-circle-fill" style="font-size: 13px;"></i> {{ message.error
-                }}
-            </li>
-          </ul>
-        </div>
+        <WarningLabel :has-error="hasError" :errors="filteredErrors" />
         <form class="d-flex flex-column gap-2 mt-auto mb-auto" @submit.prevent="handleLogin">
           <FormInput type="text" label="Username or Email" placeholder="Username or Email Address" class="form-control"
             id="userName" autofocus v-model="form.username" />
