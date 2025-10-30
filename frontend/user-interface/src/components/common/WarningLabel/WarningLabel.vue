45deefad9421762a65116942f531acd05b17b4e7
beirs_dev <beirs-dev@proton.me>
2025-10-30 18:15:42 +0800

    updated layout for login page

--- frontend/user-interface/src/components/common/WarningLabel/WarningLabel.vue
+++ frontend/user-interface/src/components/common/WarningLabel/WarningLabel.vue
@@ -16,7 +16,7 @@ defineProps({
 
 </script>
 <template>
-  <div class="alert alert-danger" role="alert" v-show="hasError">
+  <div class="alert alert-danger" :class="{ 'mb-4': hasError }" role="alert" v-show="hasError">
     <ul class="bg-white rounded p-2 mb-1 list-group-flush">
       <li class="list-group-item mb-1" style="font-size: 13px;" v-for="message in errors"><i
           class="bi bi-exclamation-circle-fill" style="font-size: 13px;"></i> {{ message.error
