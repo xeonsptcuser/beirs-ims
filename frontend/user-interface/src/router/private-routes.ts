c2f322841ac9261f732b067e4afd7519060bbc3e
beirs_dev <beirs-dev@proton.me>
2025-11-03 16:04:32 +0800

    added routes and screen components in route/index.js

--- frontend/user-interface/src/router/private-routes.ts
+++ frontend/user-interface/src/router/private-routes.ts
@@ -1,4 +1,9 @@
+import BlotterReports from '@/views/Private/BlotterReports/BlotterReports.vue'
+import Certifications from '@/views/Private/Certifications/Certifications.vue'
 import Dashboard from '@/views/Private/Dashboard/Dashboard.vue'
+import HeatMaps from '@/views/Private/HeatMaps/HeatMaps.vue'
+import Residents from '@/views/Private/Residents/Residents.vue'
+
 import type { RouteRecordRaw } from 'vue-router'
 
 export const privateRoutes: RouteRecordRaw[] = [
@@ -13,6 +18,46 @@ export const privateRoutes: RouteRecordRaw[] = [
         props: (route) => ({ role: route.params.role as string }),
         meta: {
           title: 'Dashboard',
+          requiresAuth: true,
+        },
+      },
+      {
+        path: 'residents',
+        name: 'Residents',
+        component: Residents,
+        props: (route) => ({ role: route.params.role as string }),
+        meta: {
+          title: 'View Residents',
+          requiresAuth: true,
+        },
+      },
+      {
+        path: 'certifications',
+        name: 'Certifications',
+        component: Certifications,
+        props: (route) => ({ role: route.params.role as string }),
+        meta: {
+          title: 'Certification Requests',
+          requiresAuth: true,
+        },
+      },
+      {
+        path: 'blotter-reports',
+        name: 'BlotterReports',
+        component: BlotterReports,
+        props: (route) => ({ role: route.params.role as string }),
+        meta: {
+          title: 'Blotter Reports',
+          requiresAuth: true,
+        },
+      },
+      {
+        path: 'heat-maps',
+        name: 'HeatMaps',
+        component: HeatMaps,
+        props: (route) => ({ role: route.params.role as string }),
+        meta: {
+          title: 'View Heat Map',
           requiresAuth: true,
         },
       },
---
+++ frontend/user-interface/src/views/Private/HeatMaps/HeatMaps.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+defineProps<{ role: string }>();
+
+</script>
+<template>
+  <div class="my-5 vh-100">
+    <div class="alert alert-danger" role="alert">
+      HEATMAPS VIEW PAGE HERE.... 🎉sasa Logged in as "{{ role }}"
+    </div>
+  </div>
+</template>
