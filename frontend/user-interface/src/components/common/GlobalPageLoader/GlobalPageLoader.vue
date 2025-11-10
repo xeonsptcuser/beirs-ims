b5ffc2e3abf169ce097b6a097419e8bf1b9911d8
beirs_dev <beirs-dev@proton.me>
2025-11-10 12:14:09 +0800

    increased size loading spinner size

--- frontend/user-interface/src/components/common/GlobalPageLoader/GlobalPageLoader.vue
+++ frontend/user-interface/src/components/common/GlobalPageLoader/GlobalPageLoader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 <template>
   <div class="global-loader-overlay">
-    <output class="spinner-border text-primary" aria-live="polite">
+    <output class="spinner-border text-danger" style="width: 4rem; height: 4rem;" aria-live="polite">
       <span class="visually-hidden">Loading...</span>
     </output>
     <p class="text-light mt-3 fs-5">Loading, please wait...</p>
@@ -12,7 +12,7 @@
 .global-loader-overlay {
   position: fixed;
   inset: 0;
-  background: rgba(255, 255, 255, 0.211);
+  background: rgba(255, 255, 255, 0.464);
   display: flex;
   flex-direction: column;
   justify-content: center;
