d14bdd641ce33f6e0b8fd68e180d647c99193d0c
beirs_dev <beirs-dev@proton.me>
2025-11-24 20:01:23 +0800

    added db:seed --force

--- backend/api/scripts/start.sh
+++ backend/api/scripts/start.sh
@@ -14,7 +14,7 @@ echo "Running migrations..."
 php artisan migrate --force
 
 echo "Seeding database..."
-php artisan migrate:fresh --seed --seeder=BarangayAddressSeeder
+php artisan db:seed --force --class=BarangayAddressSeeder
 
 echo "Starting Nginx..."
 nginx -g "daemon off;"
