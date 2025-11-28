196584d7bf11a89e62eab42a505e63d8556f43d6
beirs_dev <beirs-dev@proton.me>
2025-11-28 10:04:01 +0800

    fixed importing pdf files

--- frontend/user-interface/src/views/Private/Certifications/components/PdfPreviewCertificateRequest.vue
+++ frontend/user-interface/src/views/Private/Certifications/components/PdfPreviewCertificateRequest.vue
@@ -4,8 +4,8 @@ import { PDFDocument, StandardFonts } from 'pdf-lib'
 import { endpoints } from '@/services/api/endpoints'
 import { PdfRelatedService } from '@/services/api/http/pdf-service'
 import certBrgyClearancePdf from '@/assets/images/pdf/cert-clearance.pdf'
-import certIndigencyPdf from '@/assets/images/pdf/cert-clearance.pdf'
-import certResidencyPdf from '@/assets/images/pdf/cert-clearance.pdf'
+import certIndigencyPdf from '@/assets/images/pdf/cert-indigency.pdf'
+import certResidencyPdf from '@/assets/images/pdf/cert-residency.pdf'
 
 
 const props = defineProps<{
