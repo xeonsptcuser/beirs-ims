75c0ab14fedffba3155e62465f1e5d3117538da7
beirs_dev <beirs-dev@proton.me>
2025-11-30 13:22:46 +0800

    updated heatmap filter ui

--- frontend/user-interface/src/views/Private/HeatMaps/HeatMaps.vue
+++ frontend/user-interface/src/views/Private/HeatMaps/HeatMaps.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import brgyMap from '../../../assets/images/alang2-map.png'
 import { useHeatMap, type CaseType } from './composable/useHeatMap';
+import { orderedOptions } from '@/Utils/helpers/formatters';
+import { useBlotterReports } from '../BlotterReports/composable/useBlotterReport';
+import FormCheckboxInput from '@/components/common/FormCheckboxInput/FormCheckboxInput.vue';
 
 const imageURL = brgyMap
 const imageWidth = 1650
 const imageHeight = 1500
 
 const { initializeMap, drawHeatmap, fetchSections, isLoadingSections, sectionsError } = useHeatMap();
+const { incidentTypeOptions } = useBlotterReports();
+
 const currentType = ref<CaseType>("total");
 
+
+const incidentTypes = computed(() => {
+  return incidentTypeOptions.map(incidentType => incidentType.label)
+})
+
+const orderedIncidentType = computed(() => {
+  return orderedOptions(incidentTypes.value)
+})
+
 const setType = (type: CaseType) => {
-  currentType.value = type;
-  drawHeatmap(type);
+  currentType.value = type
+  drawHeatmap(currentType.value);
 }
 
 onMounted(async () => {
@@ -27,13 +41,45 @@ onMounted(async () => {
   <div class=" my-5 ">
     <div class="container">
       <div class="row my-2">
-        <div class="col-md-5 col-12 bg-white">
-          Filters and legends show here
-        </div>
         <div class="col-md-7 col-12">
           <div class="status" v-if="isLoadingSections">Loading heatmap data...</div>
           <div class="status error" v-else-if="sectionsError">{{ sectionsError }}</div>
           <div id="map" style="height: 750px;"></div>
+        </div>
+        <div class="col-md-5 col-12 bg-white">
+          <div class="px-3">
+            <section class="mt-5">
+              <p class="h5 text-center mb-4">Legends</p>
+              <div class="d-flex align-items-center justify-content-between col-8 mx-auto">
+                <div class="d-flex align-items-center">
+                  <div class="border bg-level-low" style="width: 20px; height: 20px;"></div>
+                  <p class="mb-0 ms-2">Low</p>
+                </div>
+                <div class="d-flex align-items-center">
+                  <div class="border bg-level-medium" style="width: 20px; height: 20px;"></div>
+                  <p class="mb-0 ms-2">Moderate</p>
+                </div>
+                <div class="d-flex align-items-center">
+                  <div class="border bg-level-high" style="width: 20px; height: 20px;"></div>
+                  <p class="mb-0 ms-2">High</p>
+                </div>
+              </div>
+            </section>
+            <section style="margin-top: 3rem;">
+              <p class="h5 text-center">Filters</p>
+              <hr class="col-9 mx-auto">
+              <div class="row">
+                <div class="mt-2">
+                  <ul class="list-group-flush">
+                    <li class="list-group-item mb-1" v-for="(incidentType, index) in orderedIncidentType">
+                      <FormCheckboxInput :label="incidentType" :id="index.toString()" />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </section>
+
+          </div>
         </div>
       </div>
     </div>
@@ -80,5 +126,17 @@ onMounted(async () => {
   color: #666;
   font-size: 0.8rem;
   text-align: right;
+}
+
+.bg-level-low {
+  background-color: rgb(255, 234, 2);
+}
+
+.bg-level-medium {
+  background-color: rgb(255, 175, 2);
+}
+
+.bg-level-high {
+  background-color: rgb(255, 91, 2);
 }
 </style>
