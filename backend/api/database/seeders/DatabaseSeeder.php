fe10812604503d834e3992fb62124b33cc588e4c
Riz Michael Sim <sim.rizmichael@gmail.com>
2026-02-25 09:43:19 +0800

    add seeders for users and brgy only

--- backend/api/database/seeders/DatabaseSeeder.php
+++ backend/api/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UsersSeeders::class,
-            CertificateSeeders::class,
-            BlotterReportSeeder::class,
             BarangayAddressSeeder::class,
         ]);
     }
